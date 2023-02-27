e63a91d6076d59802d3aad3728a0b1139e3f7495
Vartan Kokoghlanian <vartan.kokoghlanian@shodo-nantes.io>
2023-02-27 11:53:24 +0100

    add promotion to basket

--- src/main/java/io/shodo/BasketInformations.java
+++ src/main/java/io/shodo/BasketInformations.java
@@ -11,14 +11,24 @@ public class BasketInformations {
     // The product of the basket
     static HashMap<String, Integer> map = new HashMap<String, Integer>();
 
-    public void addProductToBasket(String product, Integer price) {
-        map.put(product, price);
+    // The fact that the basket has promo code
+    private static boolean codeDePromotion = false;
+
+    public void addProductToBasket(String product, Integer price, boolean isPromoCode) {
+        if (isPromoCode) {
+            codeDePromotion = true;
+        } else {
+            map.put(product, price);
+        }
     }
 
     public Long getBasketPrice(boolean inCents) {
         Integer v = 0;
         for (String s : map.keySet()) {
             v += map.get(s);
+        }
+        if (codeDePromotion) {
+            v -= 100;
         }
         return inCents ? v * 100 : Long.valueOf(v);
     }
