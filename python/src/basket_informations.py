d7f0d6fa893888f29e8b7e536484d56dfc1c6497
Thomas Carpaye <thomas.carpaye@gmail.com>
2023-04-14 12:23:07 +0200

    [Python] Add promotion to basket

--- python/src/basket_informations.py
+++ python/src/basket_informations.py
@@ -2,13 +2,21 @@ class BasketInformations:
     # The product of the basket
     map = {}
 
-    def add_product_to_basket(self, product, price):
-        BasketInformations.map[product] = price
+    # The fact that the basket has promo code
+    code_de_promotion = False
+
+    def add_product_to_basket(self, product, price, is_promo_code):
+        if is_promo_code:
+            BasketInformations.code_de_promotion = True
+        else:
+            BasketInformations.map[product] = price
 
     def get_basket_price(self, in_cents):
         v = 0
         for s in BasketInformations.map.values():
             v += s
+        if BasketInformations.code_de_promotion:
+            v -= 100
         return v * 100 if in_cents else Long(v)
 
     def reset_basket(self):
